2b80bb6df677f8e4c5cb8fa77d19430ed0d31417
Abel Allison <abel.allison@gmail.com>
2019-04-28 09:04:27 -0700

    nginx sync script

---
+++ bin/install-nginx-conf.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(dirname "$0")
+
+sudo rsync -azv --delete \
+    ${SCRIPT_DIR}/../nginx/sites-available \
+    /etc/nginx/sites-available
--- bin/sync.sh
+++ bin/sync.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$(dirname "$0")
-echo $SCRIPT_DIR
 
 rsync -azPv --dry-run --delete ${SCRIPT_DIR}/.. pi:~/backpack
