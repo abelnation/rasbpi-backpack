766fc4932bca2bc199dcbd56943ba693763a1d04
Abel Allison <abel.allison@gmail.com>
2019-04-28 11:05:46 -0700

    fix monitoring install

--- monitoring/install.sh
+++ monitoring/install.sh
@@ -3,5 +3,5 @@
 SCRIPT_DIR=$(dirname "$0")
 
 sudo cp ${SCRIPT_DIR}/grafana.ini /etc/grafana/grafana.ini
-sudo cp ${SCRIPT_DIR}/telegraf.ini /etc/telegraf/telegraf.conf
+sudo cp ${SCRIPT_DIR}/telegraf.conf /etc/telegraf/telegraf.conf
 sudo cp ${SCRIPT_DIR}/influxdb.conf /etc/influxdb/influxdb.conf
